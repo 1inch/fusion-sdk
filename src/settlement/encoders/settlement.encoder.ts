9521a7518ff87e293ed7763f1e30b9846bc3242a
Kirill <krboktv@gmail.com>
2023-02-17 19:39:48 +0400

    feat(settlement): add data param encoding for settleOrders

--- src/settlement/encoders/settlement.encoder.ts
+++ src/settlement/encoders/settlement.encoder.ts
@@ -4,12 +4,47 @@ import {SETTLE_ORDERS_SELECTOR} from './constants'
 import {add0x, trim0x} from '../../utils'
 import {encodeFillOrder} from './fill-order.encoder'
 
-export function encodeSettleOrders(params: FillOrderParamsExtended): string {
-    const encodedOrder = encodeFillOrder(params).substring(10)
+export function encodeSettleOrders(data: string): string {
+    data = data.substring(2)
+
+    const len = data.length
+    const offset = '20'.padStart(64, '0')
+    const bytesLenHex = (len / 2).toString(16).padStart(64, '0')
+
+    if (len % 64 === 0) {
+        return SETTLE_ORDERS_SELECTOR + offset + bytesLenHex + data
+    }
 
     return (
         SETTLE_ORDERS_SELECTOR +
-        trim0x(abiCoder.encodeParameters(['bytes'], [add0x(encodedOrder)]))
+        offset +
+        bytesLenHex +
+        data.padEnd(len + (64 - (len % 64)), '0')
+    )
+}
+
+export function encodeSettleOrdersParam(
+    params: FillOrderParamsExtended
+): string {
+    const encodedOrder = encodeFillOrder(params).substring(10)
+
+    const offsetLen = 64
+    const bytesLenLen = 64
+
+    const dataParam = trim0x(
+        abiCoder.encodeParameters(['bytes'], [add0x(encodedOrder)])
+    )
+    const bytesLen = parseInt(
+        dataParam.substring(offsetLen, offsetLen + bytesLenLen),
+        16
+    )
+
+    return (
+        '0x' +
+        dataParam.substring(
+            offsetLen + bytesLenLen,
+            offsetLen + bytesLenLen + bytesLen * 2
+        )
     )
 }
 
