57a4317081d3eaedbdc9d0d5a6b13e3d7e983196
rharutyunyan <rado9408@gmail.com>
2023-03-02 16:08:22 +0400

    feat(order-struct): Change cancel order param

--- src/sdk/encoders/cancel-order.encoder.ts
+++ src/sdk/encoders/cancel-order.encoder.ts
@@ -1,13 +1,13 @@
 import Contract from 'web3-eth-contract'
 import LimitOrderV3ABI from '../../abi/AggregationRouterV5.abi.json'
 import {AbiItem} from 'web3-utils'
-import {CancelOrderParams} from '../types'
+import {LimitOrderV3Struct} from '../../limit-order'
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const limitOrderV3 = new Contract(LimitOrderV3ABI as AbiItem[])
 
-export function encodeCancelOrder(params: CancelOrderParams): string {
+export function encodeCancelOrder(params: LimitOrderV3Struct): string {
     return limitOrderV3.methods
         .cancelOrder(
             params.salt,
