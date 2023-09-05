0fea25b546055bd39b0302953651e27f425f55ba
frolovdev <frolovandrey752@gmail.com>
2023-09-05 21:10:14 +0400

    fix: auth errors

--- src/connector/http/axios-provider.connector.ts
+++ src/connector/http/axios-provider.connector.ts
@@ -1,37 +1,54 @@
+import {AuthError} from '../../errors'
 import {HttpProviderConnector} from './http-provider.connector'
-import axios from 'axios'
+import axios, {isAxiosError} from 'axios'
 
 export class AxiosProviderConnector implements HttpProviderConnector {
     constructor(private readonly authKey?: string) {}
 
     async get<T>(url: string): Promise<T> {
-        const res = await axios.get<T>(
-            url,
-            this.authKey
-                ? {
-                      headers: {
-                          Authorization: `Bearer ${this.authKey}`
+        try {
+            const res = await axios.get<T>(
+                url,
+                this.authKey
+                    ? {
+                          headers: {
+                              Authorization: `Bearer ${this.authKey}`
+                          }
                       }
-                  }
-                : undefined
-        )
+                    : undefined
+            )
 
-        return res.data
+            return res.data
+        } catch (error) {
+            if (isAxiosError(error) && error.response?.status === 401) {
+                throw new AuthError()
+            }
+
+            throw error
+        }
     }
 
     async post<T>(url: string, data: unknown): Promise<T> {
-        const res = await axios.post<T>(
-            url,
-            data,
-            this.authKey
-                ? {
-                      headers: {
-                          Authorization: `Bearer ${this.authKey}`
+        try {
+            const res = await axios.post<T>(
+                url,
+                data,
+                this.authKey
+                    ? {
+                          headers: {
+                              Authorization: `Bearer ${this.authKey}`
+                          }
                       }
-                  }
-                : undefined
-        )
+                    : undefined
+            )
 
-        return res.data
+            return res.data
+        } catch (error) {
+            if (isAxiosError(error) && error.response?.status === 401) {
+                throw new AuthError()
+            }
+
+            throw error
+        }
     }
 }
