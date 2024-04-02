dc449b706a6135f0ad8e664a35f99e91c76ebb6a
Vladimir Borovik <v.borovik@1inch.io>
2024-04-02 19:58:37 +0300

    feat(fusion-order): fix amount calculation

--- src/sdk/utils.ts
+++ src/sdk/utils.ts
@@ -1,11 +1,15 @@
+const FEE_BASE = 100_000n
+const BPS_BASE = 10_000n
+const BPS_TO_RATIO_NUMERATOR = FEE_BASE / BPS_BASE
+
 export function bpsToRatioFormat(bps?: number): bigint {
     if (!bps) {
         return 0n
     }
 
-    return BigInt(bps) * 10n
+    return BigInt(bps) * BPS_TO_RATIO_NUMERATOR
 }
 
 export function addRatioToAmount(amount: bigint, ratio: bigint): bigint {
-    return amount + (amount * ratio) / 1_000_000_000n
+    return amount + (amount * ratio) / FEE_BASE
 }
