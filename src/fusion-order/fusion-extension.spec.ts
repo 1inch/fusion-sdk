0e6c7da05f580c88c9fe664744230f54cb0c612f
Vladimir Borovik <v.borovik@1inch.io>
2024-04-12 16:40:26 +0300

    fix(api): add extension to api response

---
+++ src/fusion-order/fusion-extension.spec.ts
@@ -0,0 +1,54 @@
+import {Address} from '@1inch/limit-order-sdk'
+import {FusionOrder} from './fusion-order'
+import {AuctionDetails} from './auction-details'
+import {FusionExtension} from './fusion-extension'
+
+describe('FusionExtension', () => {
+    it('should decode', () => {
+        const extensionContract = new Address(
+            '0x8273f37417da37c4a6c3995e82cf442f87a25d9c'
+        )
+
+        const order = FusionOrder.new(
+            extensionContract,
+            {
+                makerAsset: new Address(
+                    '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+                ),
+                takerAsset: new Address(
+                    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+                ),
+                makingAmount: 1000000000000000000n,
+                takingAmount: 1420000000n,
+                maker: new Address(
+                    '0x00000000219ab540356cbb839cbe05303d7705fa'
+                ),
+                salt: 10n
+            },
+            {
+                auction: new AuctionDetails({
+                    duration: 180n,
+                    startTime: 1673548149n,
+                    initialRateBump: 50000,
+                    points: [
+                        {
+                            coefficient: 20000,
+                            delay: 12
+                        }
+                    ]
+                }),
+                whitelist: [
+                    {
+                        address: new Address(
+                            '0x00000000219ab540356cbb839cbe05303d7705fa'
+                        ),
+                        allowFrom: 0n
+                    }
+                ],
+                resolvingStartTime: 1673548139n
+            }
+        )
+        const fusionExtension = FusionExtension.decode(order.extension.encode())
+        expect(fusionExtension).toStrictEqual(order.fusionExtension)
+    })
+})
--- src/fusion-order/fusion-extension.ts
+++ src/fusion-order/fusion-extension.ts
@@ -4,27 +4,70 @@ import {
     ExtensionBuilder,
     Interaction
 } from '@1inch/limit-order-sdk'
+import assert from 'assert'
+
 import {AuctionDetails} from './auction-details'
 import {SettlementPostInteractionData} from './settlement-post-interaction-data'
 
-export class FusionExtension extends ExtensionBuilder {
+export class FusionExtension {
+    private readonly builder = new ExtensionBuilder()
+
     constructor(
         public readonly address: Address,
         public readonly auctionDetails: AuctionDetails,
         public readonly postInteractionData: SettlementPostInteractionData
     ) {
-        super()
-    }
-
-    public build(): Extension {
         const detailsBytes = this.auctionDetails.encode()
 
-        this.withMakingAmountData(this.address, detailsBytes)
+        this.builder
+            .withMakingAmountData(this.address, detailsBytes)
             .withTakingAmountData(this.address, detailsBytes)
             .withPostInteraction(
                 new Interaction(this.address, this.postInteractionData.encode())
             )
+    }
 
-        return super.build()
+    /**
+     * Create `FusionExtension` from bytes
+     *
+     * @param bytes 0x prefixed bytes
+     */
+    public static decode(bytes: string): FusionExtension {
+        const extension = Extension.decode(bytes)
+
+        const settlementContract = Address.fromFirstBytes(
+            extension.makingAmountData
+        )
+
+        assert(
+            Address.fromFirstBytes(extension.takingAmountData).equal(
+                settlementContract
+            ) &&
+                Address.fromFirstBytes(extension.postInteraction).equal(
+                    settlementContract
+                ),
+            'Invalid extension, all calls should be to the same address'
+        )
+
+        const auctionDetails = AuctionDetails.fromExtension(extension)
+
+        const postInteractionData =
+            SettlementPostInteractionData.fromExtension(extension)
+
+        return new FusionExtension(
+            settlementContract,
+            auctionDetails,
+            postInteractionData
+        )
+    }
+
+    withMakerPermit(tokenFrom: Address, permitData: string): this {
+        this.builder.withMakerPermit(tokenFrom, permitData)
+
+        return this
+    }
+
+    public build(): Extension {
+        return this.builder.build()
     }
 }
