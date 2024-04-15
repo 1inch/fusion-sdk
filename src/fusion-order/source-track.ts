57397bb9d9cddee12f75c5531dc380ffec8acdd9
Vladimir Borovik <v.borovik@1inch.io>
2024-04-15 19:49:49 +0300

    test

--- src/fusion-order/source-track.ts
+++ src/fusion-order/source-track.ts
@@ -1,10 +1,11 @@
 import {BitMask, BN} from '@1inch/byte-utils'
-import {keccak256} from 'ethers'
+import {id} from 'ethers'
+import {add0x} from '../utils'
 
 const TRACK_CODE_MASK = new BitMask(224n, 256n)
 
 function getTrackCodeForSource(source: string): bigint {
-    return BigInt(keccak256(source).slice(10))
+    return BigInt(add0x(id(source).slice(0, 10)))
 }
 
 export function addTrackCode(salt: bigint, source: string): bigint {
