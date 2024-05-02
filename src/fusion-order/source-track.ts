2fbad1e6bdce10488dd00ddf8d77c83d4f00c94c
Kirill Kuznetcov <krboktv@gmail.com>
2024-05-02 17:05:53 +0400

    chore: don't hash hashed source

--- src/fusion-order/source-track.ts
+++ src/fusion-order/source-track.ts
@@ -1,10 +1,15 @@
 import {BitMask, BN} from '@1inch/byte-utils'
 import {id} from 'ethers'
 import {add0x} from '../utils'
+import {isHexString} from '../validations'
 
 const TRACK_CODE_MASK = new BitMask(224n, 256n)
 
 function getTrackCodeForSource(source: string): bigint {
+    if (isHexString(source) && source.length === 10) {
+        return BigInt(source)
+    }
+
     return BigInt(add0x(id(source).slice(0, 10)))
 }
 
