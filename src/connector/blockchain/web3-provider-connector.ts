4ce2398c521d00e5a3df3afa8e22d57a0db5d58a
Vladimir Borovik <v.borovik@1inch.io>
2024-07-01 15:51:30 +0300

    deps: fix security issues

--- src/connector/blockchain/web3-provider-connector.ts
+++ src/connector/blockchain/web3-provider-connector.ts
@@ -1,10 +1,7 @@
 import {BlockchainProviderConnector} from './blockchain-provider.connector'
 import Web3 from 'web3'
 import {EIP712TypedData} from '../../limit-order'
-
-interface ExtendedWeb3 extends Web3 {
-    signTypedDataV4(walletAddress: string, typedData: string): Promise<string>
-}
+import {Eip712TypedData} from 'web3-types'
 
 export class Web3ProviderConnector implements BlockchainProviderConnector {
     constructor(protected readonly web3Provider: Web3) {}
@@ -13,19 +10,9 @@ export class Web3ProviderConnector implements BlockchainProviderConnector {
         walletAddress: string,
         typedData: EIP712TypedData
     ): Promise<string> {
-        const extendedWeb3: ExtendedWeb3 = this.web3Provider.extend({
-            methods: [
-                {
-                    name: 'signTypedDataV4',
-                    call: 'eth_signTypedData_v4',
-                    params: 2
-                }
-            ]
-        })
-
-        return extendedWeb3.signTypedDataV4(
+        return this.web3Provider.eth.signTypedData(
             walletAddress,
-            JSON.stringify(typedData)
+            typedData as Eip712TypedData
         )
     }
 
--- src/nonce-manager/utils.ts
+++ src/nonce-manager/utils.ts
@@ -1,11 +1,10 @@
-import abiCoder from 'web3-eth-abi'
+import {decodeParameter} from 'web3-eth-abi'
 import {NONCE_SELECTOR} from './constants'
 import {add0x} from '../utils'
 
 export function encodeNonce(address: string): string {
     return add0x(`${NONCE_SELECTOR}${address.substring(2).padStart(64, '0')}`)
 }
-
 export function decodeNonce(nonceHex: string): string {
-    return abiCoder.decodeParameter('uint256', nonceHex).toString()
+    return (decodeParameter('uint256', nonceHex) as bigint).toString()
 }
