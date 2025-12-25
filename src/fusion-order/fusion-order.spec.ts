183dc47ff4cab91c3a6c6b419a28e54d7f8dd075
Vladimir Borovik <v.borovik@1inch.io>
2025-12-25 17:11:44 +0300

    Update fusion-order.spec.ts

--- src/fusion-order/fusion-order.spec.ts
+++ src/fusion-order/fusion-order.spec.ts
@@ -490,6 +490,59 @@ describe('Fusion Order', () => {
         expect(userAmount).toEqual(75000000n)
         expect(surplus).toEqual(25000000n)
     })
+
+    it('should fail to create fusion order with bad surplus params', () => {
+        const extensionContract = new Address(
+            '0x8273f37417da37c4a6c3995e82cf442f87a25d9c'
+        )
+
+        expect(() =>
+            FusionOrder.new(
+                extensionContract,
+                {
+                    makerAsset: new Address(
+                        '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+                    ),
+                    takerAsset: new Address(
+                        '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+                    ),
+                    makingAmount: 1000000000000000000n,
+                    takingAmount: 1420000000n,
+                    maker: new Address(
+                        '0x00000000219ab540356cbb839cbe05303d7705fa'
+                    ),
+                    salt: 10n
+                },
+                {
+                    auction: new AuctionDetails({
+                        duration: 180n,
+                        startTime: 1673548149n,
+                        initialRateBump: 50000,
+                        points: [
+                            {
+                                coefficient: 20000,
+                                delay: 12
+                            }
+                        ]
+                    }),
+                    whitelist: Whitelist.new(1673548139n, [
+                        {
+                            address: new Address(
+                                '0x00000000219ab540356cbb839cbe05303d7705fa'
+                            ),
+                            allowFrom: 0n
+                        }
+                    ]),
+                    surplus: new SurplusParams(
+                        1420000000n - 1n,
+                        Bps.fromFraction(0.1)
+                    )
+                }
+            )
+        ).toThrow(
+            'order.takingAmount must be less then surplusParams.estimatedTakerAmount'
+        )
+    })
 })
 
 describe('FusionOrder Native', () => {
