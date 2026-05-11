e11bf5cb932ba7c697e671910e28d5c1b37efb12
Mitko Tschimev <mitko@dev-thought.cool>
2026-05-11 11:06:00 +0400

    lint: format code

--- src/errors.ts
+++ src/errors.ts
@@ -1,5 +1,7 @@
 export class AuthError extends Error {
     constructor() {
-        super('Auth error, please use token from https://business.1inch.com/portal')
+        super(
+            'Auth error, please use token from https://business.1inch.com/portal'
+        )
     }
 }
